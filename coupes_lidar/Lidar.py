0981a2f7b537bcff495a90f2fa51c96ec2031397
blaiselcq <b.lecoquil@structure-rehabilitation.fr>
2020-10-01 16:07:42 +0200

    Enregistrement FIchiers de mesures

--- coupes_lidar/Lidar.py
+++ coupes_lidar/Lidar.py
@@ -5,7 +5,7 @@ from math import pi
 
 class Lidar(Thread):
     
-    def __init__(self,port,baud,data,limit = 1000):
+    def __init__(self,port,baud,data,limit = 500):
         Thread.__init__(self)
         self.lidar = RPLidar(port = port, baudrate = baud)
         try:
@@ -22,21 +22,24 @@ class Lidar(Thread):
     
     def run(self):
         time.sleep(1)
-        for measurment in self.lidar.iter_measurments(max_buf_meas=self.limit):
-            s = len(self.data[0])
-            if s >= self.limit:
-                self.data[0].pop(0)
-                self.data[1].pop(0)
-            self.data[0].append(measurment[2]*pi/180) #agle en rad
-            self.data[1].append(measurment[3]/1000) #distance en m
+        try:
+            for measurment in self.lidar.iter_measurments(max_buf_meas=self.limit):
+                s = len(self.data[0])
+                if s >= self.limit:
+                    self.data[0].pop(0)
+                    self.data[1].pop(0)
+                self.data[0].append(measurment[2]*pi/180) #agle en rad
+                self.data[1].append(measurment[3]/1000) #distance en m
+        except Exception as error:
+            print(error)
 
     def join(self, timeout=None):
         self.eteindre()
         Thread.join(self, timeout)
 
     def eteindre(self):
-        self.lidar.stop_motor()
         self.lidar.stop()
+        self.lidar.stop_motor()
         self.lidar.disconnect()
 
 if __name__ == "__main__":
--- coupes_lidar/application_coupe.py
+++ coupes_lidar/application_coupe.py
@@ -1,10 +1,13 @@
 import sys
+from os import path,access,W_OK
 import PySimpleGUI as sg
 import numpy as np
 import serial.tools.list_ports
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg
 from matplotlib.figure import Figure
 from Lidar import Lidar
+import time
+import csv
 
 listeSerie = lambda: serial.tools.list_ports.comports()
 
@@ -33,7 +36,7 @@ if __name__ == "__main__":
             ],
              [
                 sg.Text('Nombre de points :', size=(16, 1)),
-                sg.Spin(values=[_+1 for _ in range(10000)], initial_value=1000, size=(60, 1), key="nbr")
+                sg.Spin(values=[_+1 for _ in range(10000)], initial_value=500, size=(60, 1), key="nbr")
             ],
             [
                 sg.Text('Dossier cible :', size=(16, 1)),
@@ -60,8 +63,17 @@ if __name__ == "__main__":
                 pass
 
 
-            if baud != None and port != None and dossier != "" and nbr != None:
+            if port == None:
+                sg.Popup("Choisir un port")
+            elif baud not in [115200, 256000]:
+                sg.Popup("Mauvaise valeur de baud")
+            elif not path.isdir(dossier):
+                sg.Popup("Choisir un dossier valide")
+            elif type(nbr) != int or nbr not in range(1,10000):
+                sg.Popup("Choisir un nombre de points valide")
+            else: 
                 break
+                
             
         windowParam.close()
 
@@ -76,34 +88,78 @@ if __name__ == "__main__":
             print(error)
 
     
-
-    layoutScan = [
-            [sg.Canvas(size=(640, 480), key='canvas')],
-            [sg.Button('Enregistrer', size=(10, 1)),sg.Button('Annuler', size=(10, 1))]
-                ]
-    
-
-    windowScan = sg.Window('Scan Lidar', layoutScan)
-    windowScan.Finalize()
-    
-    canvas_elem = windowScan['canvas']
-    canvas = canvas_elem.TKCanvas
-
-    fig = Figure()
-    ax = fig.add_subplot(111, projection='polar')
-    ax.set_rlim(0,8)
-    fig_agg = draw_figure(canvas, fig)
-    
-    lidar.start()
-
     while True:
-        event, values = windowScan.read(timeout=1)
-        if event == 'Annuler' or event == sg.WIN_CLOSED:
-            lidar.join()
-            sys.exit()
-        ax.cla()
+        layoutScan = [
+                [sg.Canvas(size=(640, 480), key='canvas')],
+                [sg.Text('Nom du fichier :', size=(16, 1)),sg.Input(size=(30, 1),key="nomFichier")],
+                [sg.Button('Enregistrer', size=(10, 1)),sg.Button('Annuler', size=(10, 1))]
+                    ]
+        
+
+        windowScan = sg.Window('Scan Lidar', layoutScan)
+        windowScan.Finalize()
+        
+        canvas_elem = windowScan['canvas']
+        canvas = canvas_elem.TKCanvas
+
+        fig = Figure()
+        ax = fig.add_subplot(111, projection='polar')
         ax.set_rlim(0,8)
+        fig_agg = draw_figure(canvas, fig)
+        
+        lidar.start()
 
-        ax.plot(data[0],data[1],'o',markersize=1)
-        fig_agg.draw()
+        while True:
+            event, values = windowScan.read(timeout=5)
+            if event == 'Annuler' or event == sg.WIN_CLOSED:
+                lidar.join()
+                sys.exit()
+            elif event == "Enregistrer":
+                nomFichier = values['nomFichier']
 
+                if nomFichier == "" or nomFichier == None:
+                    sg.Popup("Entrer un nom de ficher")
+                else:
+                    nomComplet = path.join(dossier,nomFichier+'.csv').replace('/','\\')
+                    if path.exists(nomComplet):
+                        sg.Popup("Le fichier existe déjà")
+                    else:
+                        try:
+                            with open(nomComplet, 'x') as tempfile:
+                                pass
+                            break
+                        except OSError:
+                                sg.Popup("Le fichier ne peut pas être créé à cet emplacement.\nPermissions non accordées ou nom non valide")
+                    
+
+            ax.cla()
+            ax.set_rlim(0,8)
+
+            ax.plot(data[0],data[1],'o',markersize=1)
+            fig_agg.draw()
+
+        try:
+            lidar.join()
+        except Exception as e:
+            print(e)
+
+        windowScan.close()
+
+
+        time.sleep(5)
+
+        with open(nomComplet, 'w',newline='') as fichierData:
+            spamwriter = csv.writer(fichierData, delimiter=',',dialect='excel')
+            spamwriter.writerow(['Angle']+ ['Distance'])
+            for j in range(len(data[0])):
+                spamwriter.writerow([data[0][j],data[1][j]])
+
+        if sg.PopupOKCancel("Nouvelle mesure ?") != "OK":
+            sys.exit()
+
+        try:
+            lidar = Lidar(port,baud,data,limit=nbr)
+        except Exception as error:
+            print(error)
+            sys.exit()
+    
